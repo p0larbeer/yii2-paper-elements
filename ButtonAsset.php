baf77dc51fa9b4a1db91f7cc26cc2fddfd6d1143
p0larbeer <p0larbeer@mail.ru>
2015-07-15 04:14:39 +0300

    Add Button

---
+++ ButtonAsset.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 15.07.15
+ * Time: 4:06
+ */
+
+namespace p0larbeer\paper\elements;
+
+
+class ButtonAsset extends PaperElementsAsset
+{
+    public $href = [
+        'paper-button/paper-button.html'
+    ];
+}
--- PaperElementsAsset.php
+++ PaperElementsAsset.php
@@ -6,17 +6,27 @@
  * Time: 3:20
  */
 
-namespace p0larbeer\yii2\paper\elements;
+namespace p0larbeer\paper\elements;
 
 use yii\web\AssetBundle;
 
 class PaperElementsAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/p0larbeer/yii2-paper-elements';
+    public $sourcePath = '@vendor/p0larbeer/yii2-paper-elements/assets';
+    public $href = [];
     public $options = [
         'except'=>[
             'demo',
             'test'
         ]
     ];
+    public function registerAssetFiles($view)
+    {
+        parent::registerAssetFiles($view);
+        $manager = $view->getAssetManager();
+        foreach ($this->href  as $href) {
+            $view->registerLinkTag(['rel'=>'import','href'=>$manager->getAssetUrl($this, $href)]);
+        }
+
+    }
 }
