6e7540e5543bcfd3f42088a1269a500e11aacd22
p0larbeer <p0larbeer@mail.ru>
2015-07-15 04:33:47 +0300

    Add base widget

---
+++ Widget.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 15.07.15
+ * Time: 4:28
+ */
+
+namespace p0larbeer\paper\elements;
+
+
+
+class Widget extends \yii\base\Widget
+{
+    /**
+     * @var array the HTML attributes for the widget container tag.
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
+    public $options = [];
+
+}
