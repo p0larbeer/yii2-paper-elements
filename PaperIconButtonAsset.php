865bd5b8c307266dae44a62bee32b215c1cb2a8d
p0larbeer <p0larbeer@mail.ru>
2015-07-15 08:23:17 +0300

    add paper-icon-button asset

---
+++ PaperIconButtonAsset.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 15.07.15
+ * Time: 8:22
+ */
+
+namespace p0larbeer\paper\elements;
+
+
+class PaperIconButtonAsset extends PaperElementsAsset
+{
+    public $href = [
+        'paper-icon-button/paper-icon-button.html'
+    ];
+}
